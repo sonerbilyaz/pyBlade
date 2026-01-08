Rotation point is fixed (should be 0.25*chord)

Blade_mesher/Geometry_operations/modify.py
@@ -157,15 +157,16 @@ def change_span(points, Node_IDs_upper_surface, twist_local, taper_local):
     # Rotate the points (only x and y coordinates)
     points_origin_rotated_xy = np.dot(points_origin[:,0:2], Rz_neg.T)
     
-    ### Scale ###
-    # Scale the points to get the chord line between 0 and 1
-    points_origin_rotated_xy_scaled = points_origin_rotated_xy / chord_length
-    # Make sure that TE x coordinates are 1
-    points_origin_rotated_xy_scaled[0,0], points_origin_rotated_xy_scaled[-1,0] = 1, 1
-
+    # Center the section at the QUARTER chord location
+    ex = np.array([[1,0]])       # Unit vector in x-dir
+    points_origin_rotated_xy = points_origin_rotated_xy - ex*chord_length*0.25
+    
     """ #########################  REVERSE PROCESS #########################    """ 
-    ### Scale BACK ###
-    points_new_xy_scaled = points_origin_rotated_xy_scaled * chord_length * taper_local
+    ### Scale  ###
+    points_new_xy_scaled = points_origin_rotated_xy * taper_local
+    
+    ### Move LE to Origin ###
+    points_new_xy_scaled = points_new_xy_scaled + ex*chord_length*0.25
     
     ### Rotate BACK ###
     # 2D Rotation matrix for +z axis rotation 

Blade_mesher/run.py
@@ -14,21 +14,21 @@ from Mesher.mesh import generate_mesh
 
 """################################ INPUTS #######################################"""
 # File paths and the STEP file
-working_dir = '../Runs/12x6_ClarkY'
-stp_file = f'{working_dir}/12x6_ClarkY-1_Blade.stp'
+working_dir = '../Runs/VX4_Front_Prop'
+stp_file = f'{working_dir}/VX4_Front_Blade_single.stp'
 
 output_dir = f'{working_dir}/output'
-pts_filename = '12x6_ClarkY'
+pts_filename = 'VX4-front_prop'
 
-find_LE = True      ## Should we identify the LE ?? If so ==> True
+find_LE = False      ## Should we identify the LE ?? If so ==> True
 ###############     Panel Parameters     ###############
 ### Chordwise Distribution ###
 N_chord = 23                    ## Upper and Lower surf separately!!
 dist_airfoil = 'cosine_LE'
 
 ### Spanwise Distribution ###
-N_span= 55
-z_min, z_max = 23.5, 151.5          ## in mm
+N_span= 56
+z_min, z_max = 26, 150          ## in mm
 
 dist_spanwise = 'cosine_TIP'
 r_R = 0.82                      ## Span location to start the cosine_TIP 
@@ -38,14 +38,14 @@ remove_TE = True            # Should we remove TE ??
 close_TE = True             # Should we close the TE gap ??
 
 ### Pitch Modification (deg) ###
-collective_pitch_increment = [0,10,20]
+collective_pitch_increment = np.linspace(0,20,10)   #[0]    #np.linspace(0,20,10)
 
 ### Spanwise local Changes ###
-taper_local_root = [1,1.5]
-taper_local_tip = [1,1.5]
+chord_local_root = np.array([1])    #np.array([1])      #np.linspace(0.7,1.3,10)
+chord_local_tip = chord_local_root
 
-twist_local_root = [0,10]
-twist_local_tip = [0,0]
+twist_local_root = np.array([0])     #np.array([0])      #np.linspace(0,20,10)
+twist_local_tip = np.zeros_like(twist_local_root)
 
 ### .pts Inputs ###
 n_blades = 1        # Number of blades
@@ -54,19 +54,22 @@ rotation_center = [0, 0, 0]
 rotation_axis = [0, -1, 0]
 
 """###############################################################################"""
+
+
+
 # Create output directory if it is absent #
 if os.path.isdir(output_dir) is False:
     os.mkdir(output_dir)
 
 ### Create flag for the output file name ###
-cond_no_change = np.all(np.array(collective_pitch_increment) == 0) and np.all(np.array(taper_local_tip)==1) and np.all(np.array(taper_local_root)==1) and np.all(np.array(twist_local_root)==0) and np.all(np.array(twist_local_tip)==0)
+cond_no_change = np.all(np.array(collective_pitch_increment) == 0) and np.all(np.array(chord_local_tip)==1) and np.all(np.array(chord_local_root)==1) and np.all(np.array(twist_local_root)==0) and np.all(np.array(twist_local_tip)==0)
 
 ### Create spanwise planes ###
 z_planes = spanwise_planes(z_min, z_max, N_span, dist_spanwise, r_R)
 
 if cond_no_change:
     ###### GENERATE POINTS ######
-    ALL_sections, ALL_sections_DUST, all_sections_compound = points.get_coords(stp_file, N_chord, dist_airfoil, z_planes, close_TE, 0, twist_local=np.zeros_like(z_planes), taper_local=np.ones_like(z_planes), find_LE=find_LE)
+    ALL_sections, ALL_sections_DUST, all_sections_compound = points.get_coords(stp_file, N_chord, dist_airfoil, z_planes, close_TE, 0, twist_local=np.zeros_like(z_planes), chord_local=np.ones_like(z_planes), find_LE=find_LE)
         
     ###### GENERATE MESH AND EXPORT #######
     mesh, mesh_DUST, connectivity_DUST, coordinates_DUST = generate_mesh(ALL_sections, ALL_sections_DUST, n_blades, close_TE)
@@ -91,6 +94,8 @@ if cond_no_change:
     """#################################################################################"""    
 
     ##################################   NVLM EXPORT     ##################################
+    ALL_sections_array = np.concatenate(ALL_sections, axis=0)
+
     with open(f'{output_dir}/{pts_filename}.pts', 'w') as file:
         file.write('######## Panel parameters ########\n')
         file.write(f'type={surf_type}\n')
@@ -102,15 +107,15 @@ if cond_no_change:
         file.write('n_points={}\n'.format(len(ALL_sections[0][:,0])))
         file.write('######## End of parameters ########\n')
         
-        for i in range(len(ALL_sections)):
-            np.savetxt(file, ALL_sections[i], delimiter='\t', fmt=['%.0f','%.9f','%.9f','%.9f'], comments='')
+        ## Write the section coordinates ##                    
+        np.savetxt(file, ALL_sections_array, delimiter='\t', fmt=['%.0f','%.9f','%.9f','%.9f'], comments='')
 
     ########################################################################################        
-    ##### Write coordinates to txt to check with the paraview ######
-    with open(f'{output_dir}/Blade_points_check.txt', 'w') as file:
-        file.write('Node_ID\tX(mm)\tY(mm)\tZ(mm)\n')
-        for i in range(len(ALL_sections)):
-            np.savetxt(file, ALL_sections[i], delimiter='\t', fmt=['%.0f','%.9f','%.9f','%.9f'], comments='')
+    # ##### Write coordinates to txt to check with the paraview ######
+    # with open(f'{output_dir}/Blade_points_check.txt', 'w') as file:
+    #     file.write('Node_ID\tX(mm)\tY(mm)\tZ(mm)\n')
+    #     for i in range(len(ALL_sections)):
+    #         np.savetxt(file, ALL_sections[i], delimiter='\t', fmt=['%.0f','%.9f','%.9f','%.9f'], comments='')
 
     # ## Export all cross sections (Optional) ##
     # cq.exporters.export(all_sections_compound, f"{output_dir}/all_cross_sections.step")
@@ -118,17 +123,17 @@ if cond_no_change:
 else:
     for l in range(len(collective_pitch_increment)):
         for k in range(len(twist_local_root)):
-            for j in range(len(taper_local_root)):
+            for j in range(len(chord_local_root)):
 
-                pts_filename_new = f'{pts_filename}_pitch_{collective_pitch_increment[l]}_twist_{twist_local_root[k]}-{twist_local_tip[k]}_taper_{taper_local_root[j]}-{taper_local_tip[j]}'
+                pts_filename_new = '{}_pitch_{:.2f}_twist_{:.2f}-{:.2f}_chord_{:.2f}-{:.2f}'.format(pts_filename,float(collective_pitch_increment[l]),twist_local_root[k],twist_local_tip[k],chord_local_root[j],chord_local_tip[j])
 
-                DUST_dir = f'{working_dir}/DUST_output_pitch_{collective_pitch_increment[l]}_twist_{twist_local_root[k]}-{twist_local_tip[k]}_taper_{taper_local_root[j]}-{taper_local_tip[j]}'
+                DUST_dir = '{}/DUST_output_pitch_{:.2f}_twist_{:.2f}-{:.2f}_chord_{:.2f}-{:.2f}'.format(working_dir,collective_pitch_increment[l],twist_local_root[k],twist_local_tip[k],chord_local_root[j],chord_local_tip[j])
 
                 twist_local = np.linspace(twist_local_root[k],twist_local_tip[k], len(z_planes))
-                taper_local = np.linspace(taper_local_root[j],taper_local_tip[j], len(z_planes))
+                chord_local = np.linspace(chord_local_root[j],chord_local_tip[j], len(z_planes))
               
                 ###### GENERATE POINTS ######
-                ALL_sections, ALL_sections_DUST, all_sections_compound = points.get_coords(stp_file, N_chord, dist_airfoil, z_planes, close_TE, collective_pitch_increment[l], twist_local, taper_local, find_LE)
+                ALL_sections, ALL_sections_DUST, all_sections_compound = points.get_coords(stp_file, N_chord, dist_airfoil, z_planes, close_TE, collective_pitch_increment[l], twist_local, chord_local, find_LE)
                     
                 ###### GENERATE MESH AND EXPORT #######
                 mesh, mesh_DUST, connectivity_DUST, coordinates_DUST = generate_mesh(ALL_sections, ALL_sections_DUST, n_blades, close_TE)
@@ -152,7 +157,9 @@ else:
                 """#################################################################################"""    
 
                 ##################################   NVLM EXPORT     ##################################
-                with open(f'{output_dir}/{pts_filename}.pts', 'w') as file:
+                ALL_sections_array = np.concatenate(ALL_sections, axis=0)
+
+                with open(f'{output_dir}/{pts_filename_new}.pts', 'w') as file:
                     file.write('######## Panel parameters ########\n')
                     file.write(f'type={surf_type}\n')
                     file.write(f'n_blades={n_blades}\n\n')
@@ -162,19 +169,9 @@ else:
                     file.write('n_span_all= {}\n'.format(len(ALL_sections)))
                     file.write('n_points={}\n'.format(len(ALL_sections[0][:,0])))
                     file.write('######## End of parameters ########\n')
-                    
-                    for i in range(len(ALL_sections)):
-                        np.savetxt(file, ALL_sections[i], delimiter='\t', fmt=['%.0f','%.9f','%.9f','%.9f'], comments='')
 
-                ########################################################################################        
-                ##### Write coordinates to txt to check with the paraview ######
-                with open(f'{output_dir}/Blade_points_check.txt', 'w') as file:
-                    file.write('Node_ID\tX(mm)\tY(mm)\tZ(mm)\n')
-                    for i in range(len(ALL_sections)):
-                        np.savetxt(file, ALL_sections[i], delimiter='\t', fmt=['%.0f','%.9f','%.9f','%.9f'], comments='')
-
-                # ## Export all cross sections (Optional) ##
-                # cq.exporters.export(all_sections_compound, f"{output_dir}/all_cross_sections.step")
+                    ## Write the section coordinates ##                    
+                    np.savetxt(file, ALL_sections_array, delimiter='\t', fmt=['%.0f','%.9f','%.9f','%.9f'], comments='')
 
 ###############################################################################################################################
 end_time = timer()  # End the timer